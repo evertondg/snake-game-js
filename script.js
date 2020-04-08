0daab28f99c554858690dad371385b98ef413aee
evertondg <evertondg@hotmail.com.br>
2020-04-07 22:44:02 -0300

    feat: Definindo movimentos

--- script.js
+++ script.js
@@ -7,11 +7,16 @@ snake[0] = {
   y: 8 * box
 }
 
+let direction = "right";
+
+
 
 function criarBG() {
   context.fillStyle = "lightgreen";
   context.fillRect(0, 0, 16 * box, 16 * box);
 }
+
+
 
 
 function criarCobrinha() {
@@ -22,5 +27,32 @@ function criarCobrinha() {
 }
 
 
-criarBG();
-criarCobrinha();
+function iniciarJogo() {
+  criarBG();
+  criarCobrinha();
+
+  let snakeX = snake[0].x;
+  let snakeY = snake[0].y;
+
+
+  if (direction == 'right') snakeX += box;
+  if (direction == 'left') snakeX -= box;
+  if (direction == 'up') snakeY -= box;
+  if (direction == 'down') snakeY += box;
+
+  snake.pop();
+
+  let newHead = {
+    x: snakeX,
+    y: snakeY
+  }
+
+  snake.unshift(newHead)
+  console.log('teste');
+}
+
+
+
+
+
+let jogo = setInterval(iniciarJogo, 1000)
